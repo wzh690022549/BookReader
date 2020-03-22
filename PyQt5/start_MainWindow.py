c717831312196aecb19b72a702de43b5437f56ca
wang <690022549@qq.com>
2020-03-23 00:26:13 +0800

    学习使用PyQt5制作UI界面

---
+++ PyQt5/start_MainWindow.py
@@ -0,0 +1,11 @@
+import sys
+import MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = MainWindow.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
